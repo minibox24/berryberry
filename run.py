ff730a43fab20898f24fb007ec3abecd260ae78b
minibox24 <minibox724@gmail.com>
2021-05-04 17:59:46 +0900

    ✅ 개발 환경 구축

---
+++ run.py
@@ -0,0 +1,26 @@
+# StrawberryDonut/Interpreter commit 47c31c7 modification version
+
+import asyncio
+
+from interpreter import Parser, Execute
+from interpreter.Execute import toString
+
+
+code = """
+import "local:main"
+
+bb = BerryBerry()
+"""
+
+
+async def main():
+    exe = Execute.ExecuteClass({})
+
+    parse_res = Parser.parse(code)
+
+    execute_res = await exe.execute(parse_res)
+    if execute_res:
+        print(toString(execute_res))
+
+
+asyncio.run(main())
